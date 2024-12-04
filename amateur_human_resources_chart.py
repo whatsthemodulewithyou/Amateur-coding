ce5232d8c5e9671d881ef82bda4aa18788f6b5bf
whatsthemodulewithyou <john.f.vicente11@gmail.com>
2024-12-04 11:11:54 -0800

    Add files via upload
    This file contains a series of basic Python functions designed to handle and display human resource-related data, such as employee names, IDs, ages, and addresses. Each function focuses on extracting and returning specific information to show fundamental understanding of Python programming concepts.

---
+++ amateur_human_resources_chart.py
@@ -0,0 +1,24 @@
+def get_names(attendance):
+    people = attendance
+    return people
+attendance = ["tom", "dick", "harry"]
+print(get_names(attendance))
+
+def get_numbers(employee_id):
+    id_numbers = employee_id
+    return id_numbers
+employee_id = [122903, 593020, 432321 ]
+print((get_numbers(employee_id)))
+
+def get_age(employee_age):
+    age_chart = employee_age
+    return age_chart
+employee_age = ["age: 20","age: 30","age: 31"]
+print((employee_age))
+
+def get_address(employee_address):
+    address_chart = employee_address
+    return address_chart
+employee_address = ["561 Wallaby.St", "321 Decending.Ln","123 Acending.Ln"]
+print(get_address(employee_address))
+
