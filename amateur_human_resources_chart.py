711ee8dae3a37a4564b3cf8cd11e28fb54ec6918
whatsthemodulewithyou <john.f.vicente11@gmail.com>
2024-12-09 12:31:18 -0800

    Updated amateur_human_resources_chart.py
    Better readability. The function has an intention revealing name.

--- amateur_human_resources_chart.py
+++ amateur_human_resources_chart.py
@@ -1,28 +1,26 @@
-def get_names(attendance):
-    people = attendance
-    return people
-attendance = ["tom", "dick", "harry"]
-print(get_names(attendance))
+roster = ["tom", "dick", "harry"]
+employee_id = [122903, 593020, 432321 ]
+employee_age = ["age: 20","age: 30","age: 31"]
+employee_address = ["561 Wallaby.St", "321 Decending.Ln","123 Acending.Ln"]
 
-def get_numbers(employee_id):
+def display_id(roster):
+    employee_names = roster
+    return employee_names
+print(display_id(roster))
+
+def display_empl(employee_id):
     id_numbers = employee_id
     return id_numbers
-employee_id = [122903, 593020, 432321 ]
-print((get_numbers(employee_id)))
+print((display_empl(employee_id)))
 
-def get_age(employee_age):
+def display_age(employee_age):
     age_chart = employee_age
     return age_chart
-employee_age = ["age: 20","age: 30","age: 31"]
-print((employee_age))
+print(display_empl(employee_age))
 
-def get_address(employee_address):
+def display_adress(employee_address):
     address_chart = employee_address
     return address_chart
-employee_address = ["561 Wallaby.St", "321 Decending.Ln","123 Acending.Ln"]
-print(get_address(employee_address))
-
-##The datatypes used in this are integers also known as int and strings also known as str.
-## All functions are named based on what they do, using an action-oriented naming convention with a verb prefix like "get".
+print(display_adress(employee_address))
 
 
