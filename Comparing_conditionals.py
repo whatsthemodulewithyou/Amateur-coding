d4dfae908be1bea339b7578d3776d473b7130b62
whatsthemodulewithyou <john.f.vicente11@gmail.com>
2024-12-14 22:38:52 -0800

    Add files via upload

---
+++ Comparing_conditionals.py
@@ -0,0 +1,22 @@
+a = 23
+b = 42
+
+if a > b :
+    print("a is more than b")
+elif a == b :
+    print("a is equal to b")
+else:
+    print("a is less than b")
+
+if b > a :
+    print("b is more than a")
+elif b == a :
+    print("b is equal to a")
+else:
+    print("b is less than a")
+
+
+#Comparing variables. These are 3 conditional branches. Why? Because (a > b = b < a), (a == b = b==a), (a < b = b > a). they are reciprocals!
+#Same type of branch , different perspective. this is like saying 9x3 is different as 3x9 in a logical sense.
+
+
