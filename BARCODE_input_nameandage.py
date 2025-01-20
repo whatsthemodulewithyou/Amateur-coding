aca68af2d9a94f34910a1260b9635a4b6767ca3b
John Vicente <john.f.vicente11@gmail.com>
2025-01-19 16:09:51 -0800

    Add files via upload
    Barcode ;)

---
+++ BARCODE_input_nameandage.py
@@ -0,0 +1,12 @@
+user_name = input('Enter you name')
+first_name = str(user_name)
+
+users_age = input('Enter your age')
+age = int(users_age)
+
+if  age >= 21:
+    print("You can legally drink alchohol")
+elif age < 21:
+    print("You cannot legally drink alchohol buddy... get some ginger ale instead")
+
+
